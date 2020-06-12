2057ba0dd9b53b8cb61d723acca634410256aa29
nikolasavic <nikolasav@gmail.com>
2020-06-12 16:32:33 +0200

    - initial commit

---
+++ src/main/java/com/start/client/MainApp.java
@@ -0,0 +1,55 @@
+package com.start.client;
+
+import com.dncomponents.UiField;
+import com.dncomponents.client.components.HasCellComponents;
+import com.dncomponents.client.components.core.HtmlBinder;
+import com.dncomponents.client.components.core.entities.ItemId;
+import com.dncomponents.client.components.sidemenu.SideMenu;
+import com.dncomponents.client.dom.History;
+import com.dncomponents.client.views.IsElement;
+import com.dncomponents.client.views.appview.AcceptsOneElement;
+import com.dncomponents.client.views.appview.PlaceManager;
+import com.start.client.cell.CellPlace;
+import com.start.client.greeting.GreetingPlace;
+import com.start.client.home.HomePlace;
+import elemental2.dom.HTMLElement;
+import elemental2.dom.Node;
+
+public class MainApp implements AcceptsOneElement {
+
+    @UiField
+    HTMLElement contentWrapper;
+    @UiField
+    public SideMenu<ItemId> side;
+
+    PlaceManager placeManager = new PlaceManager(this);
+    HtmlBinder binder = HtmlBinder.get(MainApp.class, this);
+
+    public MainApp() {
+        binder.bind();
+        init();
+        side.setPlaceManager(placeManager);
+        side.expandAll(false);
+    }
+
+    public void init() {
+        placeManager.register(HomePlace.HomePlaceRegister.instance);
+        placeManager.register(GreetingPlace.GreetingPlaceRegister.instance);
+        placeManager.register(CellPlace.CellPlaceRegister.instance);
+        placeManager.setHomePlace(HomePlace.class);
+        History.fireCurrentHistoryState();
+    }
+
+    @Override
+    public void setElement(IsElement element) {
+        contentWrapper.innerHTML = "";
+        contentWrapper.appendChild(element.asElement());
+        if (element instanceof HasCellComponents) {
+            ((HasCellComponents) element).resetScrollPosition();
+        }
+    }
+
+    public Node asElement() {
+        return binder.getTemplate().getCloned();
+    }
+}
